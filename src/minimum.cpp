a382a1be921346a4eca70e21fa044447e4bfecdb
yasu80 <p111026h@mail.kyutech.jp>
2017-09-05 18:01:18 +0900

    add comment
    やり過ぎ?

--- src/minimum.cpp
+++ src/minimum.cpp
@@ -9,24 +9,24 @@
 class ImageConverter
 {
   ros::NodeHandle nh_ {};
-  ros::NodeHandle pnh_ {"~"};
-  image_transport::ImageTransport it_ {nh_};
-  image_transport::ImageTransport pit_ {pnh_};
-  image_transport::Subscriber image_sub_ {it_.subscribe("input_image", 1, &ImageConverter::imageCb, this)};
-  image_transport::Publisher image_pub_ {pit_.advertise("image_raw", 1)};
+  ros::NodeHandle pnh_ {"~"};//ノードハンドル(プライベート)
+  image_transport::ImageTransport it_ {nh_};//itpデータクラスの作成:ノードハンドルを渡して初期化
+  image_transport::ImageTransport pit_ {pnh_};//itpデータクラスの作成:プライベートノードハンドルを渡して初期化
+  image_transport::Subscriber image_sub_ {it_.subscribe("input_image", 1, &ImageConverter::imageCb, this)};//this = Imageconverter,input:コールバック処理"imageCb"の実行
+  image_transport::Publisher image_pub_ {pit_.advertise("image_raw", 1)};//image_rawの発信
 
 public:
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)//コールバック処理:これのポインタを渡して実行するように作られている
   {
     try {
-      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);//一旦openCV形式にコンバート
       // Draw an example circle on the video stream
-      if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-        cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+      if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)//画像サイズの確認
+        cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));//唯一のOpenCV処理:円形描画
 
       // Output modified video stream
-      image_pub_.publish(cv_ptr->toImageMsg());
-    } catch (cv_bridge::Exception& e) {
+      image_pub_.publish(cv_ptr->toImageMsg());//ポインタを渡してパブリッシュメソッドを実行(パブリッシュ)
+    } catch (cv_bridge::Exception& e) {//例外処理
       ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
     }
   }
