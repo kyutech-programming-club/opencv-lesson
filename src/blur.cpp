e6f7163f92662a6aba3b656bb581d42ef08d1178
tanacchi <q111026d@mail.kyutech.jp>
2017-09-19 22:54:36 +0900

    Remove extra header

--- src/blur.cpp
+++ src/blur.cpp
@@ -1,10 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 
 class ImageBlurer {
 public:
