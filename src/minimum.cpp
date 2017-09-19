0c5dc975e5f20f045b0c073861567f696ee48b20
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-20 00:15:46 +0900

    Change node name
    Change node name and change to auto from explicit type

--- src/minimum.cpp
+++ src/minimum.cpp
@@ -7,15 +7,15 @@
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
+  ros::init(argc, argv, "minimum_node");
 
   ros::NodeHandle nh {};
   ros::NodeHandle pnh {"~"};//ノードハンドル(プライベート)
   image_transport::ImageTransport it {nh};//ImageTransportデータクラスの作成:ノードハンドルを渡して初期化
   image_transport::ImageTransport pit {pnh};//ImageTransportデータクラスの作成:プライベートノードハンドルを渡して初期化
-  image_transport::Publisher image_pub {pit.advertise("image_raw", 1)};//image_rawトピックへの吐き出しを指定して, データサイズを"1"に指定
+  auto image_pub {pit.advertise("image_raw", 1)};//image_rawトピックへの吐き出しを指定して, データサイズを"1"に指定
 
-  image_transport::Subscriber image_sub {it.subscribe<sensor_msgs::Image>("input_image", 1, [&image_pub](const sensor_msgs::ImageConstPtr& msg){
+  auto image_sub {it.subscribe<sensor_msgs::Image>("input_image", 1, [&image_pub](const sensor_msgs::ImageConstPtr& msg){
     try {
       auto cv_ptr {cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8)}; // openCV形式にコンバート
       // Draw an example circle on the video stream
