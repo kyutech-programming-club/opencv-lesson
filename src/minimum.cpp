c24d7fd594aec9adcda78f2109e55305f99c10e2
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-20 00:05:28 +0900

    Update minimum_node with lambda function object

--- src/minimum.cpp
+++ src/minimum.cpp
@@ -5,18 +5,15 @@
 
 #include <opencv2/imgproc.hpp>
 
-class ImageConverter
+int main(int argc, char** argv)
 {
-  ros::NodeHandle nh_ {};
-  ros::NodeHandle pnh_ {"~"};//ノードハンドル(プライベート)
-  image_transport::ImageTransport it_ {nh_};//ImageTransportデータクラスの作成:ノードハンドルを渡して初期化
-  image_transport::ImageTransport pit_ {pnh_};//ImageTransportデータクラスの作成:プライベートノードハンドルを渡して初期化
-  image_transport::Subscriber image_sub_ {it_.subscribe("input_image", 1, &ImageConverter::imageCb, this)};//thisはあるクラス内でそのクラス自身を指す参照(ポインタなど),これを介してROSトピックによるinputに付随するコールバック処理"imageCb"の実行
-  image_transport::Publisher image_pub_ {pit_.advertise("image_raw", 1)};//image_rawトピックへの吐き出しを指定して, データサイズを"1"に指定
-
-public:
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)//15行目で指定したコールバック処理の具体的な記載:
-  {
+  ros::init(argc, argv, "image_converter");
+  ros::NodeHandle nh {};
+  ros::NodeHandle pnh {"~"};//ノードハンドル(プライベート)
+  image_transport::ImageTransport it {nh};//ImageTransportデータクラスの作成:ノードハンドルを渡して初期化
+  image_transport::ImageTransport pit {pnh};//ImageTransportデータクラスの作成:プライベートノードハンドルを渡して初期化
+  image_transport::Publisher image_pub {pit.advertise("image_raw", 1)};//image_rawトピックへの吐き出しを指定して, データサイズを"1"に指定
+  image_transport::Subscriber image_sub {it.subscribe<sensor_msgs::Image>("input_image", 1, [&image_pub](const sensor_msgs::ImageConstPtr& msg){
     try {
       cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);//openCV形式にコンバート
       // Draw an example circle on the video stream
@@ -28,13 +25,8 @@ public:
     } catch (cv_bridge::Exception& e) {//例外処理
       ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
     }
-  }
-};
+  })};
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic {};
   ros::spin();
   return 0;
 }
