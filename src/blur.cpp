f25c3996ea0ae713a4553dd9d18c867590693dba
tanacchi <q111026d@mail.kyutech.jp>
2017-09-19 23:28:12 +0900

    Add header needed

--- src/blur.cpp
+++ src/blur.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <opencv2/improc.hpp>
 
 class ImageBlurer {
 public:
