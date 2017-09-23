c9349dce70227106e6de242366911f8788904083
marubashi11 <marubashi.yuuki641@mail.kyutech.jp>
2017-09-23 17:19:51 +0900

    Checkout minimum.cpp from master

--- src/minimum.cpp
+++ src/minimum.cpp
@@ -5,39 +5,28 @@
 
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp> //inRange用
-
-#define LOW_H 18 / 2 //Hは0~360の値を2で割る
-#define LOW_S 0.5 * 255 //Sは0~1の値に255を掛ける
-#define LOW_V 0 * 255 //VもSに同じ
-#define UP_H 36 / 2
-#define UP_S 1 * 255
-#define UP_V 1 * 255
 
 class ImageConverter
 {
   ros::NodeHandle nh_ {};
-  ros::NodeHandle pnh_ {"~"};
-  image_transport::ImageTransport it_ {nh_};
-  image_transport::ImageTransport pit_ {pnh_};
-  image_transport::Subscriber image_sub_ {it_.subscribe("input_image", 1, &ImageConverter::imageCb, this)};
-  image_transport::Publisher image_pub_ {pit_.advertise("image_raw", 1)};
+  ros::NodeHandle pnh_ {"~"};//ノードハンドル(プライベート)
+  image_transport::ImageTransport it_ {nh_};//ImageTransportデータクラスの作成:ノードハンドルを渡して初期化
+  image_transport::ImageTransport pit_ {pnh_};//ImageTransportデータクラスの作成:プライベートノードハンドルを渡して初期化
+  image_transport::Subscriber image_sub_ {it_.subscribe("input_image", 1, &ImageConverter::imageCb, this)};//thisはあるクラス内でそのクラス自身を指す参照(ポインタなど),これを介してROSトピックによるinputに付随するコールバック処理"imageCb"の実行
+  image_transport::Publisher image_pub_ {pit_.advertise("image_raw", 1)};//image_rawトピックへの吐き出しを指定して, データサイズを"1"に指定
 
 public:
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)//15行目で指定したコールバック処理の具体的な記載:
   {
     try {
-      cv::Mat mask_img;
-      cv::Mat hsv_img;
-      cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-      cv_bridge::CvImageConstPtr mask_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);//openCV形式にコンバート
+      // Draw an example circle on the video stream
+      if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)//画像サイズがこれから書き込む↓のcircleに満足かを確認
+        cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));//唯一のOpenCV処理:円形描画
 
-      cv::cvtColor(cv_ptr->image, hsv_img, CV_BGR2HSV);
-      cv::inRange(hsv_img, cv::Scalar(LOW_H, LOW_S, LOW_V), cv::Scalar(UP_H, UP_S, UP_V), mask_img); //オレンジ色検出(閾値要調整)
-      cv::cvtColor(mask_img, mask_ptr->image, CV_GRAY2BGR, 3); //mask_img(1ch)を3chのMat画像(mask_ptr->image)に変換
-      image_pub_.publish(mask_ptr->toImageMsg());
-    }
-    catch (cv_bridge::Exception& e) {
+      // Output modified video stream
+      image_pub_.publish(cv_ptr->toImageMsg());//cv_ptrの指すMat構造体(openCV形式)をMessage(ROS形式)にしてパブリッシュ
+    } catch (cv_bridge::Exception& e) {//例外処理
       ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
     }
   }
