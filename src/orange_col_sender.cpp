720f920079965ac2275af35dab59acc76c39530c
tanacchi <q111026d@mail.kyutech.jp>
2017-10-07 16:29:48 +0900

    Update orange_col_sender

--- src/orange_col_sender.cpp
+++ src/orange_col_sender.cpp
@@ -5,10 +5,12 @@
 
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
-#include "std_msgs/UInt32MultiArray.h"
+#include "std_msgs/Int32MultiArray.h"
 
 #include <opencv2/imgproc.hpp>  // for cvtColor
 #include <opencv2/opencv.hpp>   // for inRange
+
+#include <stdio.h>
 
 class OrangeColSender
 {
@@ -16,19 +18,23 @@ class OrangeColSender
   ros::NodeHandle pnh_{"~"};
   image_transport::ImageTransport it_{nh_};
   image_transport::Subscriber sub_{it_.subscribe("mask_image", 1, &OrangeColSender::send_col, this)};
-  ros::Publisher pub_{nh_.advertise<std_msgs::UInt32MultiArray>("orange_col", 1000)};
+  ros::Publisher pub_{nh_.advertise<std_msgs::Int32MultiArray>("orange_col", 1000)};
 public:
   OrangeColSender() = default;
   void send_col(const sensor_msgs::ImageConstPtr& msg)
   {
     try {
-      cv_bridge::CvImageConstPtr img_ptr{cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)};
-      std_msgs::UInt32MultiArray dest{};
+      cv_bridge::CvImageConstPtr img_ptr{cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)};
+      std_msgs::Int32MultiArray dest{};
+      const int begin{0}, square_size{15}, end{img_ptr->image.rows-square_size};
+      for (int y{begin}; y < end; ++y)
+        for (int x{0}; x < img_ptr->image.cols-square_size; ++x)
+          if (cv::mean(cv::Mat{img_ptr->image, cv::Rect{x, y, square_size, square_size}})[0] > 200) dest.data.push_back(x);
       pub_.publish(dest);
     }
     catch (cv_bridge::Exception& e) {
       ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
-    }
+    } 
   }
 };
 
